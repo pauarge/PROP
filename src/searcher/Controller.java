8a185fc932771e26b2b9e64c2102e65aae58ef8e
Pau Argelaguet <pau@outlook.com>
2016-04-24 16:19:40 +0200

    Setting up project

--- src/searcher/Controller.java
+++ src/searcher/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package searcher;
 
 public class Controller {
 }
--- src/searcher/Main.java
+++ src/searcher/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package searcher;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
