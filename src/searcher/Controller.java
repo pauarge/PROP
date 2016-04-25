cda202882641cd4f7561c7a145701a33b338fd44
carlos-marchal <marchal.carlos@gmail.com>
2016-04-25 19:22:44 +0200

    He netejat molt el controlador, hauria de ser senzill implementar la resta de coses. Podeu provar la consola a ver que us sembla la interficie que estic fent.

--- src/searcher/Controller.java
+++ src/searcher/Controller.java
@@ -1,87 +1,66 @@
 package searcher;
 
+import common.domain.Container;
 import common.domain.Graph;
+import common.domain.Node;
 import common.domain.NodeType;
 
 public class Controller {
 
-    private static final String GLOBAL_HELP = "Global help";
-    private static final String COMMAND_NOT_FOUND = "Comanda no reconeguda. Escriu 'help', o una comanda seguit de 'help' per l'ajuda.";
-    private static final String ADD_HELP = "Add help";
+    private static final String GLOBAL_HELP = "add\nprint\nquit";
+    private static final String ADD_HELP = "add node nodeType [nodeId] nodeName";
+    private static final String PRINT_HELP = "print node";
+    private static final String PRINT_NODE_HELP = "print node all\nprint node type1 [type2 ...]";
 
     private Graph graph;
+
+    private boolean readyToQuit = false;
+
+    private NodeType getType(String type) {
+        if (type == null) return null;
+        switch (type) {
+            case "author":
+                return NodeType.AUTHOR;
+            case "paper":
+                return NodeType.PAPER;
+            case "conference":
+                return NodeType.CONF;
+            case "term":
+                return NodeType.TERM;
+            case "label":
+                return NodeType.LABEL;
+            default:
+                return null;
+        }
+    }
+
+    private String getFirstWord(String line) {
+        if (line == null) {
+            return "";
+        } else {
+            return line.split("\\s+", 2)[0].toLowerCase();
+        }
+    }
+
+    private String getRestOfWords(String line) {
+        if (line == null) {
+            return "";
+        } else {
+            String[] words = line.split("\\s+", 2);
+            if (words.length == 1) {
+                return "";
+            } else {
+                return words[1];
+            }
+        }
+    }
 
     Controller() {
         graph = new Graph();
     }
 
-    private String getFirstWord(String line) {
-        return line.split("\\s+", 2)[0];
-    }
-
-    private String getRestOfWords(String line) {
-        String[] words = line.split("\\s+", 2);
-        if (words.length == 1) {
-            return null;
-        } else {
-            return words[1];
-        }
-    }
-
-    private String addNode(String line) {
-        String type = getFirstWord(line);
-        String value = getRestOfWords(line);
-
-        NodeType nodeType;
-        switch (type) {
-            case "author":
-                nodeType = NodeType.AUTHOR;
-                break;
-            case "paper":
-                nodeType = NodeType.PAPER;
-                break;
-            case "conference":
-                nodeType = NodeType.CONF;
-                break;
-            case "term":
-                nodeType = NodeType.TERM;
-                break;
-            case "label":
-                nodeType = NodeType.LABEL;
-                break;
-            default:
-                return COMMAND_NOT_FOUND;
-        }
-
-        if (value == null || value.equals("")) {
-            return COMMAND_NOT_FOUND;
-        }
-
-        if (value.matches("\\d+\\s+.*")) {
-            value = value.replaceFirst("\\d+\\s+", "");
-        }
-
-        graph.createNode(nodeType, value);
-        return "Node added successfully.";
-    }
-
-    private String executeAdd(String line) {
-        return null;
-
-        //TODO: Arreglar els problemes amb punters nuls
-        /*String command = getFirstWord(line);
-        String parameters = getRestOfWords(line);
-
-        if (parameters == null) {
-            return ADD_HELP;
-        }
-
-        switch (command) {
-            case "node":
-                return addNode(parameters);
-            default:
-                return ADD_HELP;
-        }*/
+    boolean isReadyToQuit() {
+        return readyToQuit;
     }
 
     String executeLine(String line) {
@@ -89,17 +68,108 @@ public class Controller {
         String parameters = getRestOfWords(line);
 
         switch (command) {
+            case "add":
+                return executeAdd(parameters);
+            case "print":
+                return executePrint(parameters);
             case "":
                 return null;
             case "help":
                 return GLOBAL_HELP;
             case "quit":
-                System.exit(0);
-            case "add":
-                return executeAdd(parameters);
+                this.close();
+                return null;
             default:
-                return COMMAND_NOT_FOUND;
+                return "Comanda no reconeguda. Escriu 'help' per obtenir un llistat de comandes valides";
         }
+    }
+
+    private String executeAdd(String line) {
+        String command = getFirstWord(line);
+        String parameters = getRestOfWords(line);
+
+        switch (command) {
+            case "node":
+                return addNode(parameters);
+            default:
+                return ADD_HELP;
+        }
+    }
+
+    private String addNode(String line) {
+        String type = getFirstWord(line);
+        String value = getRestOfWords(line);
+
+        NodeType nodeType = getType(type);
+
+        if (nodeType == null) {
+            return type + " no es un tipus valid de node!";
+        }
+
+        if (value == null || value.equals("")) {
+            return "El node ha de tenir un nom!";
+        }
+
+        if (value.matches("\\d+\\s+.*")) {
+            value = value.replaceFirst("\\d+\\s+", "");
+        }
+
+        Node node = graph.createNode(nodeType, value);
+        try {
+            graph.addNode(node);
+            return "Node added successfully.";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "EXCEPTION RAISED!";
+        }
+    }
+
+    private String executePrint(String line) {
+        String command = getFirstWord(line);
+        String parameters = getRestOfWords(line);
+
+        switch (command) {
+            case "node":
+                return printNode(parameters);
+            default:
+                return PRINT_HELP;
+        }
+    }
+
+    private String printNode(String parameter) {
+        boolean didSomething = false;
+        String[] args = parameter.split("\\s+");
+        if (args[0].equals("all")) {
+            args = new String[] {"author", "conference", "paper", "label", "term"};
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String arg : args) {
+            NodeType nt = getType(arg);
+            if (nt != null) {
+                sb.append('\n' + arg + ":\n");
+                sb.append(printAllNodeType(nt));
+                didSomething = true;
+            }
+        }
+        if (didSomething) {
+            return sb.toString();
+        } else {
+            return PRINT_NODE_HELP;
+        }
+    }
+
+    private String printAllNodeType(NodeType nodeType) {
+        StringBuilder sb = new StringBuilder();
+        Container.ContainerIterator it = graph.getNodeIterator(nodeType);
+        while (it.hasNext()) {
+            Node node = (Node) it.next();
+            sb.append(node.getId() + "    " + node.getValue() + '\n');
+        }
+        return sb.toString();
+    }
+
+    private void close() {
+        readyToQuit = true;
     }
 
 }
--- src/searcher/Main.java
+++ src/searcher/Main.java
@@ -13,14 +13,16 @@ public class Main {
 
         String readLine;
         String outputLine;
-        System.out.println("Benvigut al projecte de prop del grup 18-?. Escriu 'help' per l'ajuda, o 'quit' per sortir.");
+        System.out.println("Projecte de prop del grup 18-?. Escriu 'help' per l'ajuda, o 'quit' per sortir.");
         try {
             System.out.print(">");
             while ((readLine = bufferedReader.readLine()) != null) {
-                readLine = readLine.toLowerCase();
                 outputLine = controller.executeLine(readLine);
                 if (outputLine != null) {
                     System.out.println(outputLine);
+                }
+                if (controller.isReadyToQuit()) {
+                    System.exit(0);
                 }
                 System.out.print(">");
             }
