e19001887bca687047f5afc4d81f8584c44c59bf
carlos-marchal <marchal.carlos@gmail.com>
2016-04-25 09:57:54 +0200

    Treballant en la interficie

--- src/searcher/Controller.java
+++ src/searcher/Controller.java
@@ -1,16 +1,25 @@
 package searcher;
 
+import common.domain.Graph;
+import common.domain.NodeType;
+
 public class Controller {
 
-    static final private String GLOBAL_HELP = "This is the help placeholder.";
+    private static final String GLOBAL_HELP = "Global help";
+    private static final String COMMAND_NOT_FOUND = "Comanda no reconeguda. Escriu 'help', o una comanda seguit de 'help' per l'ajuda.";
+    private static final String ADD_HELP = "Add help";
 
-    Controller() {}
+    private Graph graph;
 
-    private String getCommand(String line) {
+    Controller() {
+        graph = new Graph();
+    }
+
+    private String getFirstWord(String line) {
         return line.split("\\s+", 2)[0];
     }
 
-    private String getParams(String line) {
+    private String getRestOfWords(String line) {
         String[] words = line.split("\\s+", 2);
         if (words.length == 1) {
             return null;
@@ -19,9 +28,65 @@ public class Controller {
         }
     }
 
+    private String addNode(String line) {
+        String type = getFirstWord(line);
+        String value = getRestOfWords(line);
+
+        NodeType nodeType;
+        switch (type) {
+            case "author":
+                nodeType = NodeType.AUTHOR;
+                break;
+            case "paper":
+                nodeType = NodeType.PAPER;
+                break;
+            case "conference":
+                nodeType = NodeType.CONF;
+                break;
+            case "term":
+                nodeType = NodeType.TERM;
+                break;
+            case "label":
+                nodeType = NodeType.LABEL;
+                break;
+            default:
+                return COMMAND_NOT_FOUND;
+        }
+
+        if (value == null || value.equals("")) {
+            return COMMAND_NOT_FOUND;
+        }
+
+        if (value.matches("\\d+\\s+.*")) {
+            value = value.replaceFirst("\\d+\\s+", "");
+        }
+
+        graph.createNode(nodeType, value);
+        return "Node added successfully.";
+    }
+
+    private String executeAdd(String line) {
+        return null;
+
+        //TODO: Arreglar els problemes amb punters nuls
+        /*String command = getFirstWord(line);
+        String parameters = getRestOfWords(line);
+
+        if (parameters == null) {
+            return ADD_HELP;
+        }
+
+        switch (command) {
+            case "node":
+                return addNode(parameters);
+            default:
+                return ADD_HELP;
+        }*/
+    }
+
     String executeLine(String line) {
-        String command = getCommand(line);
-        String parameters = getParams(line);
+        String command = getFirstWord(line);
+        String parameters = getRestOfWords(line);
 
         switch (command) {
             case "":
@@ -30,8 +95,10 @@ public class Controller {
                 return GLOBAL_HELP;
             case "quit":
                 System.exit(0);
+            case "add":
+                return executeAdd(parameters);
             default:
-                return "Comanda no reconeguda. Escriu 'help' per l'ajuda.";
+                return COMMAND_NOT_FOUND;
         }
     }
 
