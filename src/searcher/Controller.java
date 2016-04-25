6a5596fa3dac44d6112075a639db89b142f7d32f
carlos-marchal <marchal.carlos@gmail.com>
2016-04-25 02:16:00 +0200

    Main acabat, falta implementar el controlador.

--- src/searcher/Controller.java
+++ src/searcher/Controller.java
@@ -2,16 +2,36 @@ package searcher;
 
 public class Controller {
 
-    Controller() {
+    static final private String GLOBAL_HELP = "This is the help placeholder.";
 
+    Controller() {}
+
+    private String getCommand(String line) {
+        return line.split("\\s+", 2)[0];
+    }
+
+    private String getParams(String line) {
+        String[] words = line.split("\\s+", 2);
+        if (words.length == 1) {
+            return null;
+        } else {
+            return words[1];
+        }
     }
 
     String executeLine(String line) {
-        switch (line) {
+        String command = getCommand(line);
+        String parameters = getParams(line);
+
+        switch (command) {
+            case "":
+                return null;
             case "help":
-                return "Pagina d'ajuda.";
+                return GLOBAL_HELP;
+            case "quit":
+                System.exit(0);
             default:
-                return "Comanda no reconeguda. Escriu help per l'ajuda.";
+                return "Comanda no reconeguda. Escriu 'help' per l'ajuda.";
         }
     }
 
