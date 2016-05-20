95ce56b34283a563d134357e37fa4b0ee94e3db0
Pau Argelaguet <pau@outlook.com>
2016-05-20 12:37:52 +0200

    Added Graph export

--- src/searcher/controllers/MainController.java
+++ src/searcher/controllers/MainController.java
@@ -7,10 +7,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 
@@ -19,18 +25,40 @@ public class MainController implements Initializable {
     private Graph graph;
     private PersistenceController pc;
     @FXML private Button returnRoot;
+    @FXML private Button exportGraph;
 
     @FXML
     public void returnRootAction() throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("../layouts/landing.fxml"));
         Stage stage = (Stage) returnRoot.getScene().getWindow();
-        stage.setScene(new Scene(root));
-        stage.show();
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Dialog");
+        alert.setHeaderText("Estàs segur de tornar a la finestra principal?");
+        alert.setContentText("Fent això, es descartaran els canvis no exportats.");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            Parent root = FXMLLoader.load(getClass().getResource("../layouts/landing.fxml"));
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+
+    }
+
+    public void exportGraphAction(){
+        Stage stage = (Stage) exportGraph.getScene().getWindow();
+        DirectoryChooser dc = new DirectoryChooser();
+        final File selectedDirectory = dc.showDialog(stage);
+        if(selectedDirectory != null) {
+            System.out.println("Starting graph export...");
+            pc.exportGraph(selectedDirectory.getAbsolutePath());
+            System.out.println("Graph export finished.");
+        }
     }
 
     public void importDir(String path){
-        System.out.println("Importing graph...");
+        System.out.println("Starting graph import...");
         pc.importGraph(path);
+        System.out.println("Graph import finished.");
     }
 
     @Override
