cea7b78e0f0ee0bd81e23f9e92865cb21d08d3b4
Pau Argelaguet <pau@outlook.com>
2016-05-23 11:50:43 +0200

    Added method on BaseController

--- src/searcher/controllers/BaseController.java
+++ src/searcher/controllers/BaseController.java
@@ -1,9 +1,9 @@
 package searcher.controllers;
 
-
 import common.domain.Graph;
 import common.persistence.PersistenceController;
 import javafx.fxml.Initializable;
+
 
 public abstract class BaseController implements Initializable {
 
@@ -12,6 +12,10 @@ public abstract class BaseController implements Initializable {
 
     public void setGraph(Graph g){
         graph = g;
+    }
+
+    public void setPersistenceController(PersistenceController pc){
+        this.pc = pc;
     }
 
 }
