8aee5d8f86534d7bdf1a8a6d927b75231b4c5d31
carlos-marchal <marchal.carlos@gmail.com>
2016-05-31 00:24:41 +0200

    arestes

--- src/searcher/controllers/popups/EdgesViewController.java
+++ src/searcher/controllers/popups/EdgesViewController.java
@@ -6,6 +6,7 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import searcher.Utils;
@@ -15,24 +16,89 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class EdgesViewController extends BaseController {
+    @FXML private ChoiceBox<Relation> choiceRelation;
+    @FXML private Label labelTypeA;
+    @FXML private Label labelTypeB;
+
     @FXML private TextField textName;
     @FXML private ChoiceBox<NodeType> choiceNodeA;
     @FXML private ChoiceBox<NodeType> choiceNodeB;
+
+    @FXML private Button buttonDelete;
     @FXML private Button buttonNew;
+    @FXML private Button buttonClose;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        buttonNew.setOnAction(event -> addEdge());
+        choiceRelation.setItems(edgeTypes);
+        choiceRelation.setConverter(Utils.relationStringConverter());
+        choiceRelation.valueProperty().addListener((observable, oldValue, newValue) -> showRelation(newValue));
+        choiceRelation.getSelectionModel().selectFirst();
+
+        buttonDelete.setOnAction(event -> removeSelectedRelation());
+        buttonClose.setOnAction(event -> ((Stage)(textName.getScene().getWindow())).close());
+        buttonNew.setOnAction(event -> switchToAdd());
+
+        textName.textProperty().addListener((o, ov, nv) -> buttonNew.setDisable(nv.isEmpty()));
         choiceNodeA.setItems(FXCollections.observableArrayList(NodeType.class.getEnumConstants()));
         choiceNodeB.setItems(FXCollections.observableArrayList(NodeType.class.getEnumConstants()));
         choiceNodeA.setConverter(Utils.nodeTypeStringConverter());
         choiceNodeB.setConverter(Utils.nodeTypeStringConverter());
     }
 
+    private void switchToAdd() {
+        textName.setVisible(true);
+        textName.clear();
+        choiceNodeA.setVisible(true);
+        choiceNodeA.setValue(null);
+        choiceNodeA.getSelectionModel().selectFirst();
+        choiceNodeB.setVisible(true);
+        choiceNodeB.setValue(null);
+        choiceNodeB.getSelectionModel().selectFirst();
+
+        buttonNew.setText("Afegeix");
+        buttonNew.setDisable(true);
+        buttonNew.setOnAction(event -> addEdge());
+        buttonDelete.setText("Enrere");
+        buttonDelete.setDisable(false);
+        buttonDelete.setOnAction(event -> switchToBrowse());
+    }
+
+    private void switchToBrowse() {
+        textName.setVisible(false);
+        choiceNodeA.setVisible(false);
+        choiceNodeB.setVisible(false);
+        choiceRelation.getSelectionModel().selectFirst();
+
+        buttonDelete.setText("Esborra");
+        buttonDelete.setOnAction(event -> removeSelectedRelation());
+        buttonNew.setText("Nova");
+        buttonNew.setDisable(false);
+        buttonNew.setOnAction(event -> switchToAdd());
+    }
+
+    private void removeSelectedRelation() {
+        Relation relation = choiceRelation.getValue();
+        edgeTypes.remove(relation);
+        choiceRelation.getSelectionModel().selectFirst();
+    }
+
+    private void showRelation(Relation relation) {
+        if (relation == null) {
+            labelTypeA.setText("");
+            labelTypeB.setText("");
+            buttonDelete.setDisable(true);
+        } else {
+            labelTypeA.setText(Utils.getName(relation.getNodeTypeA()));
+            labelTypeB.setText(Utils.getName(relation.getNodeTypeB()));
+            buttonDelete.setDisable(relation.getId() < 6);
+        }
+    }
+
     private void addEdge() {
         Relation edge = new Relation(choiceNodeA.getValue(), choiceNodeB.getValue(), textName.getText());
         graph.addRelation(edge);
         edgeTypes.add(edge);
-        ((Stage)(textName.getScene().getWindow())).close();
+        switchToBrowse();
     }
 }
